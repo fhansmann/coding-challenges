660697e3c99a05274680ee36577e858259ca9c3e
Florian Hansmann <30904740+fhansmann@users.noreply.github.com>
2019-12-18 13:53:05 +0100

    Create c_4.py

---
+++ c_4.py
@@ -0,0 +1,11 @@
+values=input("Please enter numbers:" )
+
+l=values.split(",")
+t=tuple(l)
+
+#tuple() method can convert list to tuple 
+#remember: method is a function that is available for a given object because of the object's type.
+
+print(l)
+print(t)
+
