696cb222e89192be4f9419f0ed61689b8193d0aa
Florian Hansmann <30904740+fhansmann@users.noreply.github.com>
2019-12-22 23:27:00 +0100

    Update c_4.py

--- c_4.py
+++ c_4.py
@@ -3,11 +3,10 @@ values=input("Please enter numbers:" )
 l=values.split(",")
 t=tuple(l)
 
-#tuple() method can convert list to tuple 
-#remember: method is a function that is available for a given object because of the object's type.
-
 print(l)
 print(t)
+
+#tuple() method can convert list to tuple
 
 #Output:
 #['34', '67', '55', '33', '12', '98']
