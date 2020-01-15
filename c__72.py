f69e2bcdad63e5f4e9bc80b4421df1d3cdc41f7d
Florian Hansmann <30904740+fhansmann@users.noreply.github.com>
2020-01-15 22:10:04 +0100

    Update c__72.py

--- c__72.py
+++ c__72.py
@@ -10,3 +10,5 @@ L2 = [1, 2, 5, 6]
 
 remove_dup(L1, L2)
 print(L1)
+
+# Note that you should not modify a list as your are iterating over it; Python is not updating the list as you change the list; make a copy first!
